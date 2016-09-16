e5d2167822e45284db7e785ea2d8531c7b3a266e
Mitchell Seedorf <mitchell.seedorf@cavero.nl>
2016-09-16 13:29:16 +0200

    initial commit to remote

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,21 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { Component } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
+import { OrderByPipe } from './orderpipe';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
-})
-export class AppComponent { }
+    templateUrl: 'list.html'
+}) 
+
+export class AppComponent { 
+	    myRowData = [
+        {"name":"Ronald Bowman","badge":"4","blog":"3"},
+        {"name":"Pamela Hill","badge":"2","blog":"4"},
+        {"name":"Jogy Bermal","badge":"2","blog":"4"},
+        {"name":"Wolfram Jhonson","badge":"2","blog":"4"},
+        {"name":"Willfred King","badge":"2","blog":"4"},
+    ];
+
+    title = "Professionals - Ranking the pro's"
+}
--- app/main.ts
+++ app/main.ts
@@ -2,4 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+import { AppComponent } from './app.component';
+
+import { OrderByPipe } from './orderpipe';
+
 platformBrowserDynamic().bootstrapModule(AppModule);
